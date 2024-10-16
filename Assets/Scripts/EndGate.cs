ad915d64fb44f986fe597239679b96343c5159ac
Domino-111 <dom.walsh@outlook.com>
2024-10-16 16:29:41 +1100

    Term 2 Week 1

--- Assets/Scripts/EndGate.cs
+++ Assets/Scripts/EndGate.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class EndGate : TriggerZone
 {
+    HighScoreSystem highScore;
+
+    public void Start()
+    {
+        highScore = FindAnyObjectByType<HighScoreSystem>();
+    }
+
     public override void Activate(Collider collider)
     {
         if (Timer.instance)
         {
             Debug.Log(Timer.instance.CurrentTime());
+            highScore.NewScore(Timer.instance.CurrentTime());
         }
     }
 }
---
+++ Assets/Scripts/Saving/HighScoreData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class HighScoreData
+{
+    public float[] scores;
+    public string[] names;
+
+    //Constructor
+    public HighScoreData()
+    {
+        scores = new[] {99f, 10f, 1f};
+        names = new[] {"Andrew", "Alex", "Steve"};
+    }
+
+    public HighScoreData(float[] scores, string[] names)
+    {
+        this.scores = scores;
+        this.names = names;
+    }
+}
